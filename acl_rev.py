ebb30b7d603875aa5b1eb43766b7e326ea65303f
fess <fess.dm@gmail.com>
2019-03-22 19:45:05 +0300

    it reverses rules! added output to file (which does not work)

--- acl_rev.py
+++ acl_rev.py
@@ -2,29 +2,27 @@
 # script does not verify validity of ACL
 # it just reverses its direction
 
-import collections
+# import collections
 import re
 import sys
 
 # Parser = collections.namedtuple('ACLparser', ['name', 'pattern', 'parser'])
 
-
-# def re_compile(pattern: str):
-#     """compile case independent regex"""
-#     compiled_regex = re.compile(pattern, re.IGNORECASE)
-#     return compiled_regex
-
 acl_file = sys.stdin
 if len(sys.argv) > 1:
     file_name = sys.argv[1]
     acl_file = open(file_name)
+    output_name = file_name + '.rev'
+    print("results would be written in", output_name)
+    output_file = open(output_name, mode='w')
 else:
     print("no file specified. Reading from stdin")
-
+    output_file = sys.stdout
 
 ip_ptrn = r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
 remark_ptrn = r'.*remark (?P<remark_text>).*'
-src_ptrn = r'(?P<source>host ' + ip_ptrn + '|' + ip_ptrn + ' ' + ip_ptrn + '|any)'
+src_ptrn = r'(?P<source>host ' + ip_ptrn + '|' + \
+    ip_ptrn + ' ' + ip_ptrn + '|any)'
 dst_ptrn = r'(?P<destination>host ' + ip_ptrn + \
     '|' + ip_ptrn + ' ' + ip_ptrn + '|any)'
 rule_ptrn = r'(?P<beginning>.*) (?P<action>permit|deny) (?P<protocol>\w+) ' \
@@ -39,15 +37,33 @@ rule_ptrn = r'(?P<beginning>.*) (?P<action>permit|deny) (?P<protocol>\w+) ' \
 )
 print(remark_ptrn, rule_ptrn, ip_ptrn, sep='\n\n')
 
+
+def makeprint(file_desc):
+    """returns function to print to desired output"""
+    return lambda *param, **arg: print(*param, **arg, file=file_desc)
+
+
+printerr = makeprint(sys.stderr)
+printout = makeprint(output_file)
+
 for string in acl_file:
     string = string.rstrip()
-    print(string)
+    printerr(string)
     if remark_re.match(string):
         match = remark_re.match(string)
-        print("it's a remark!", match.group('remark_text'))
+        printerr("it's a remark!", match.group('remark_text'))
     elif rule_re.match(string):
-        print("it's a rule!")
+        match = rule_re.match(string)
+        printerr("it's a rule!")
+        processed = (match.group('beginning'), match.group('action'),
+                     match.group('protocol'),
+                     match.group('destination'), match.group('dst_port') or '',
+                     match.group('source'), match.group('src_port') or ''
+                     )
+        string = ' '.join(processed)
+        printerr(string)
     elif ip_re.match(string):
-        print("i don't know what it is, but it contains IP!")
+        printerr("i don't know what it is, but it contains IP!")
     else:
-        print("i don't khow what it is")
+        printerr("i don't khow what it is")
+    printout(string)
